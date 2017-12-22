3067a369dcdea5e82426ee031823b6d4de31ecbd
Chris Xue <chrisxue815@gmail.com>
2017-12-22 17:56:03 +0000

    Format build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
-    var kotlin_version: String by extra
-    kotlin_version = "1.2.21"
+    var kotlinVersion: String by extra
+    kotlinVersion = "1.2.21"
 
     repositories {
         mavenCentral()
     }
+
     dependencies {
-        classpath(kotlinModule("gradle-plugin", kotlin_version))
+        classpath(kotlinModule("gradle-plugin", kotlinVersion))
     }
 }
 
@@ -19,20 +20,21 @@ apply {
     plugin("kotlin")
 }
 
-val kotlin_version: String by extra
+val kotlinVersion: String by extra
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    compile(kotlinModule("stdlib-jdk8", kotlin_version))
+    compile(kotlinModule("stdlib-jdk8", kotlinVersion))
     testCompile("junit", "junit", "4.12")
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
+
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
