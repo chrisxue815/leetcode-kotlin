4a1c45bb4e5720dd43f3e71e8e4b7897ead25e5f
Chris Xue <chrisxue815@gmail.com>
2018-01-01 11:04:35 +0000

    530. Minimum Absolute Difference in BST.

---
+++ src/main/kotlin/p530.kt
@@ -0,0 +1,57 @@
+package leetcode.p530
+
+import leetcode.util.TreeNode
+import leetcode.util.deserialize
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+
+// O(n) time. O(log(n)) space. Recursive in-order traversal.
+class Solution {
+    fun getMinimumDifference(root: TreeNode?): Int {
+        var minDiff = Int.MAX_VALUE
+        var prev = -1
+
+        tailrec fun dfs(root: TreeNode?) {
+            if (root == null) {
+                return
+            }
+
+            dfs(root.left)
+
+            if (prev != -1) {
+                minDiff = Math.min(minDiff, root.`val` - prev)
+            }
+
+            prev = root.`val`
+
+            dfs(root.right)
+        }
+
+        dfs(root)
+
+        return minDiff
+    }
+}
+
+class SolutionTest {
+    @ParameterizedTest
+    @MethodSource("createTestData")
+    fun test(root: List<Int>, expected: Int) {
+        val root = root.deserialize()
+        val actual = Solution().getMinimumDifference(root)
+        assertEquals(expected, actual)
+    }
+
+    companion object {
+        @JvmStatic
+        fun createTestData(): Stream<Arguments> {
+            return Stream.of(
+                    Arguments.of(listOf(1, null, 3, 2), 1)
+            )
+        }
+    }
+}
