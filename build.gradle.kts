8ca80bbea48a8c091a677d8c6455b74f3922848c
Chris Xue <chrisxue815@gmail.com>
2020-04-09 14:55:07 -0700

    Upgrade to Kotlin 1.3.71

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,30 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-buildscript {
-    var kotlinVersion: String by extra
-    kotlinVersion = "1.2.21"
-
-    repositories {
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath(kotlinModule("gradle-plugin", kotlinVersion))
-        classpath("org.junit.platform:junit-platform-gradle-plugin:1.0.3")
-    }
+plugins {
+    kotlin("jvm") version "1.3.71"
 }
 
-version = "1.0"
-
-apply {
-    plugin("java")
-    plugin("kotlin")
-    plugin("org.junit.platform.gradle.plugin")
-}
-
-val kotlinVersion: String by extra
+group = "org.example"
+version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    compile(kotlinModule("stdlib-jdk8", kotlinVersion))
-    compile("org.junit.jupiter", "junit-jupiter-api", "5.0.3")
-    compile("org.junit.jupiter", "junit-jupiter-params", "5.0.3")
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.6.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.1")
 }
 
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    named<Test>("test") {
+        useJUnitPlatform()
+    }
 }
