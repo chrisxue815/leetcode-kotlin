9b629ae52c772836510e5ea97a4a15849cd51f94
Chris Xue <chrisxue815@gmail.com>
2020-10-25 23:18:10 -0700

    81. Search in Rotated Sorted Array II

---
+++ src/test/kotlin/leetcode/test_0081.kt
@@ -0,0 +1,68 @@
+package leetcode.test_0081
+
+import kotlinx.serialization.Serializable
+import leetcode.util.loadTestJson
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+// O(log(n)) average, O(n) worst-case time. O(1) space. Binary search.
+class Solution {
+    fun search(nums: IntArray, target: Int): Boolean {
+        var lo = 0
+        var hi = nums.size - 1
+
+        while (lo <= hi) {
+            val mid = lo + ((hi - lo) shr 1)
+
+            if (nums[mid] == target) {
+                return true
+            }
+
+            when {
+                nums[mid] < nums[lo] -> {
+                    if (nums[mid] <= target && target <= nums[hi]) {
+                        lo = mid + 1
+                    }
+                    else {
+                        hi = mid - 1
+                    }
+                }
+                nums[mid] > nums[lo] -> {
+                    if (nums[lo] <= target && target < nums[mid]) {
+                        hi = mid - 1
+                    }
+                    else {
+                        lo = mid + 1
+                    }
+                }
+                else -> {
+                    lo++
+                }
+            }
+        }
+
+        return false
+    }
+}
+
+class SolutionTest {
+    @Test
+    fun test() {
+        val testData = loadTestJson(javaClass.packageName, TestJson.serializer())
+
+        for (case in testData.test_cases) {
+            val actual = Solution().search(case.args.nums, case.args.target)
+            assertEquals(actual, case.expected)
+        }
+    }
+
+    @Serializable
+    class TestJson(val test_cases: List<TestCase>)
+
+    @Serializable
+    class TestCase(val args: Args, val expected: Boolean)
+
+    @Serializable
+    class Args(val nums: IntArray, val target: Int)
+}
