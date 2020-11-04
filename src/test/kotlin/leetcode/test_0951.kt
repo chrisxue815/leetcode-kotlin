ccceb025f632d7cedbb18bb1040b6cc35e32252c
Chris Xue <chrisxue815@gmail.com>
2020-11-03 21:28:06 -0800

    951. Flip Equivalent Binary Trees

--- src/test/kotlin/leetcode/test_0951.kt
+++ src/test/kotlin/leetcode/test_0951.kt
@@ -12,15 +12,14 @@ import kotlin.test.assertTrue
 // O(n) time. O(log(n)) space. Recursive pre-order traversal.
 class Solution {
     fun flipEquiv(root1: TreeNode?, root2: TreeNode?): Boolean {
-        if (root1 == null && root2 == null) {
-            return true
+        if (root1 == null || root2 == null) {
+            return root1 == root2
         }
-        if (root1 != null && root2 != null) {
-            return root1.`val` == root2.`val`
-                    && (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)
-                    || flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left))
+        if (root1.`val` != root2.`val`) {
+            return false
         }
-        return false
+        return flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)
+                || flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left)
     }
 }
 
