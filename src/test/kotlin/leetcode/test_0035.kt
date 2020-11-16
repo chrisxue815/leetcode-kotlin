f99debc72dc0938cd5397f2e72ec3e8d1d238794
Chris Xue <chrisxue815@gmail.com>
2020-11-15 23:08:01 -0800

    35. Search Insert Position

---
+++ src/test/kotlin/leetcode/test_0035.kt
@@ -0,0 +1,39 @@
+package leetcode.test_0035
+
+import kotlinx.serialization.Serializable
+import leetcode.util.loadTestJson
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+// O(log(n)) time. O(1) space. Binary search.
+class Solution {
+    fun searchInsert(nums: IntArray, target: Int): Int {
+        val index = nums.binarySearch(target)
+        return if (index >= 0) {
+            index
+        } else {
+            -(index + 1)
+        }
+    }
+}
+
+class SolutionTest {
+    @Test
+    fun test() {
+        val testData = loadTestJson(javaClass.packageName, TestJson.serializer())
+
+        for (case in testData.test_cases) {
+            val actual = Solution().searchInsert(case.args.nums, case.args.target)
+            assertEquals(case.expected, actual)
+        }
+    }
+
+    @Serializable
+    class TestJson(val test_cases: List<TestCase>)
+
+    @Serializable
+    class TestCase(val args: Args, val expected: Int)
+
+    @Serializable
+    class Args(val nums: IntArray, val target: Int)
+}
