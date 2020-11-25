62c7dabdcc3561d8aed937ee822749771b98bc8b
Chris Xue <chrisxue815@gmail.com>
2020-11-24 20:28:50 -0800

    495. Teemo Attacking

---
+++ src/test/kotlin/leetcode/test_0495.kt
@@ -0,0 +1,46 @@
+package leetcode.test_0495
+
+import kotlinx.serialization.Serializable
+import leetcode.util.loadTestJson
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+// O(n) time. O(1) space. Array.
+class Solution {
+    fun findPoisonedDuration(timeSeries: IntArray, duration: Int): Int {
+        if (timeSeries.isEmpty()) {
+            return 0
+        }
+
+        var result = duration
+        var prev = timeSeries[0]
+
+        for (curr in timeSeries) {
+            result += Integer.min(duration, curr - prev)
+            prev = curr
+        }
+
+        return result
+    }
+}
+
+class SolutionTest {
+    @Test
+    fun test() {
+        val testData = loadTestJson(javaClass.packageName, TestJson.serializer())
+
+        for (case in testData.test_cases) {
+            val actual = Solution().findPoisonedDuration(case.args.timeSeries, case.args.duration)
+            assertEquals(case.expected, actual)
+        }
+    }
+
+    @Serializable
+    class TestJson(val test_cases: List<TestCase>)
+
+    @Serializable
+    class TestCase(val args: Args, val expected: Int)
+
+    @Serializable
+    class Args(val timeSeries: IntArray, val duration: Int)
+}
